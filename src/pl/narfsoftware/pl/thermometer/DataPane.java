dfb1067a7511f90cea37c30118a6f93254393605
Mateusz Buda <mateusz@narfsoftware.pl>
2013-09-20 00:40:39 +0200

    ActionBar - History element

--- src/pl/narfsoftware/pl/thermometer/DataPane.java
+++ src/pl/narfsoftware/pl/thermometer/DataPane.java
@@ -19,7 +19,7 @@ public class DataPane extends ActionBarActivity
 	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.data_pane, menu);
-		return true;
+		return super.onCreateOptionsMenu(menu);
 	}
 
 }
